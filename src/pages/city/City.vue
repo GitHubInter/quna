b77cace3b0df5ba90319133092f657d4175c825e
githubinter <githubinter@outlook.com>
2021-03-18 23:17:26 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch
+  },
+  data () {
+    return {
+      cities: {}
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
