4264c149d6c6776d3155368a30113d290ac59f7c
githubinter <githubinter@outlook.com>
2021-04-03 00:21:31 +0800

    city ajax

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <city-search :cities="cities"></city-search>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 export default {
   name: 'City',
   components: {
@@ -22,8 +23,33 @@ export default {
   },
   data () {
     return {
-      cities: {}
+      cities: {},
+      hotCities: [],
+      letter: ''
     }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then((res) => {
+          // 使用es6箭头函数this指向就不会错误
+          res = res.data
+          if (res.ret && res.data) {
+            const data = res.data
+            this.cities = data.cities
+            this.hotCities = data.hotCities
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
